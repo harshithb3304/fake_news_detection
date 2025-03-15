2f586cb43546712f1f5653a4f166d71cc032c6b3
harshithb3304 <harsh03032004@gmail.com>
2025-03-16 01:15:11 +0530

    npm issue

--- fakenews_client/src/app/home/page.jsx
+++ fakenews_client/src/app/home/page.jsx
@@ -36,7 +36,7 @@ export default function FakeNewsDetector() {
     setIsLoading(true);
   
     try {
-      const response = await fetch("http://localhost:5000/analyze", {
+      const response = await fetch("https://fake-news-detection-gmue.vercel.app/analyze", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
